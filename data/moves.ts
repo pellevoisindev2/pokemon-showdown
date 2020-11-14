d16047993c2cfe4d53bd46a0d169bf8bb54e4c5d
SDM0 <60699143+SDM0@users.noreply.github.com>
2020-11-14 16:02:13 +0100

    Testing Clumsiness

--- data/moves.ts
+++ data/moves.ts
@@ -17096,8 +17096,11 @@ export const Moves: {[moveid: string]: MoveData} = {
 		pp: 10,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1, authentic: 1},
-		onHit(target) {
-			target.clearBoosts();
+		onHit(pokemon) {
+			this.add('-clearallboost');
+			for (const pokemon of this.getAllActive()) {
+				pokemon.clearBoosts();
+			}
 		},
 		secondary: null,
 		target: "normal",

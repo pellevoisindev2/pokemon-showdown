4891da781d3fa1b5fa25692260d97e85af5a4b68
SDM0 <60699143+SDM0@users.noreply.github.com>
2020-11-15 23:08:11 +0100

    Testing Cocoon Trap again

--- data/moves.ts
+++ data/moves.ts
@@ -12170,7 +12170,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 4,
 		flags: {},
 		stallingMove: true,
-		volatileStatus: 'protect',
+		volatileStatus: 'obstruct',
 		onTryHit(pokemon) {
 			return !!this.queue.willAct() && this.runEvent('StallMove', pokemon);
 		},
@@ -12201,14 +12201,14 @@ export const Moves: {[moveid: string]: MoveData} = {
 					}
 				}
 				if (move.flags['contact']) {
-					this.boost({spe: -1}, target);
+					this.boost({spe: -1}, source);
 					this.useMove("stickyweb", pokemon);
 				}
 				return this.NOT_FAIL;
 			},
 			onHit(target, source, move) {
 				if (move.isZOrMaxPowered && move.flags['contact']) {
-					this.boost({spe: -1}, target);
+					this.boost({spe: -1}, source);
 					this.useMove("stickyweb", pokemon);
 				}
 			},

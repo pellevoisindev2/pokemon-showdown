23034a94758be8438cf3188da4da605190ce6d34
Pellevoisindev <bouret.benjamin@gmail.com>
2020-12-28 14:08:09 +0100

    test

--- data/abilities.ts
+++ data/abilities.ts
@@ -5065,60 +5065,24 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
         // },
     // },
 	tracker: {
-		onStart(pokemon) {
-			pokemon.addVolatile('trackertrap');
+		onStart(target) {
+			target.addVolatile('trackertrap');
 			console.log("firstOnStart");
 		},
-		onEnd(pokemon) {
-			delete pokemon.volatiles['trackertrap'];
-			this.add('-end', pokemon, 'Tracker Trap', '[silent]');
+		onEnd(target) {
+			delete target.volatiles['trackertrap'];
+			this.add('-end', target, 'Tracker Trap', '[silent]');
 			console.log("firstOnEnd");
 		},
 		onResidualOrder: 11,
-            onResidual(pokemon) {
+        onResidual(target) {
 				console.log("firstOnResidual");
                 const source = this.effectData.source;
-                //G-Max Centiferno and G-Max Sandblast continue even after the user leaves the field
-                if (source && (!source.isActive || source.hp <= 0 || !source.activeTurns)) {
-                    delete pokemon.volatiles['trackertrap'];
-					//delete source.volatiles['trackertrap'];
-                    this.add('-end', pokemon, this.effectData.sourceEffect, '[trackertrap]', '[silent]');
-					//this.add('-end', source, this.effectData.sourceEffect, '[trackertrap]', '[silent]');
-                    return;
-                }
-            },
-		condition: {
-            duration: 1,
-            onStart(target) {
-                this.add('-start', target, 'Tracker Trap');
-				console.log("secondOnStart");
-				// this.add('-activate', source, 'Tracker Trap');
-                //this.effectData.boundDivisor = source.hasItem('bindingband') ? 6 : 8;
-            },
-            onResidualOrder: 11,
-            onResidual(target) {
-				console.log("onResidual");
-                const source = this.effectData.source;
-                //G-Max Centiferno and G-Max Sandblast continue even after the user leaves the field
                 if (source && (!source.isActive || source.hp <= 0 || !source.activeTurns)) {
                     delete target.volatiles['trackertrap'];
-					//delete source.volatiles['trackertrap'];
                     this.add('-end', target, this.effectData.sourceEffect, '[trackertrap]', '[silent]');
-					//this.add('-end', source, this.effectData.sourceEffect, '[trackertrap]', '[silent]');
                     return;
                 }
-            },
-            onEnd(target) {
-                this.add('-end', target, this.effectData.sourceEffect, '[trackertrap]');
-				target.removeVolatile('trackertrap');
-				console.log("secondOnEnd");
-				//this.add('-end', source, this.effectData.sourceEffect, '[trackertrap]');
-            },
-            onTrapPokemon(target) {
-				console.log("isTrapped");
-                //if (this.effectData.source?.isActive) target.tryTrap();
-				//if (this.effectData.source?.isActive) source.tryTrap();
-            },
         },
 		name: "Tracker",
 		rating: 2,

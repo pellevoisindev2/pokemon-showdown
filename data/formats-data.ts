a410fabe182bd65dce0f035613361de099c49528
Pellevoisindev <bouret.benjamin@gmail.com>
2021-01-13 14:40:51 +0100

    test

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -6262,6 +6262,6 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		tier: "OU",
 	},
 	spiritute: {
-		tier: "LC UU",
+		tier: "MEGAS",
 	},
 };
--- data/rulesets.ts
+++ data/rulesets.ts
@@ -383,6 +383,27 @@ export const Formats: {[k: string]: FormatData} = {
 			}
 		},
 	},
+	megas: {
+		effectType: 'ValidatorRule',
+		name: 'MEGAS',
+		desc: "Only allows Pok&eacute;mon that can evolve and don't have any prior evolutions",
+		onValidateSet(set) {
+			const species = this.dex.getSpecies(set.species || set.name);
+			if (species.prevo && this.dex.getSpecies(species.prevo).gen <= this.gen) {
+				return [set.species + " isn't the first in its evolution family."];
+			}
+			if (!species.nfe) {
+				return [set.species + " doesn't have an evolution family."];
+			}
+			// Temporary hack for LC past-gen formats and other mashups
+			if (set.level > 5) {
+				return [`${set.species} can't be above level 5 in Little Cup formats.`];
+			}
+			if (species.tier != 'MEGAS') {
+				return [set.species + " is not a MEGA."];
+			}
+		},
+	},
 	blitz: {
 		effectType: 'Rule',
 		name: 'Blitz',
